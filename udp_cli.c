9dadc7fffad0d152e4641ed3464a76822b1bcc24
vigneshtvs484 <vigneshtvs484@gmail.com>
2017-05-14 10:40:43 -0400

    Modifed client and server files to work.
    Unit-Test:
    cabox@box-codeanywhere:~/workspace$ ./udp_cli vig nic hello
    
    Remote user not found
    cabox@box-codeanywhere:~/workspace$ ./udp_cli vig vig hello
    
    hello
    cabox@box-codeanywhere:~/workspace$ ./udp_cli vig nic hello
    
    Change-Id: I50707f69c1d702816fa24e1e0059c981d362d804

---
+++ udp_cli.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 9999
+
+int main(int argc, char *argv[])
+{
+	int client, i;
+	struct sockaddr_in ser_addr, remaddr;
+        socklen_t addrlen = sizeof(remaddr);
+	char buff[1024] = {0};
+	
+	if (argc < 4) {
+		printf("\n Not enough args passed udp_cli <your name> <remote name> \"<message>\"\n");
+		return -1;
+        }
+
+	client = socket(AF_INET, SOCK_DGRAM, 0);
+	if(client < 0) {
+		perror("socket:");
+		return -1;
+	}
+	
+	memset((char *) &remaddr, 0, sizeof(remaddr));
+	remaddr.sin_family = AF_INET;
+	remaddr.sin_port = htons(PORT);
+	if (inet_aton("127.0.0.1", &remaddr.sin_addr)==0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+	
+	for (i = 1; i <= argc - 1; i++) {
+		strcat(buff, argv[i]);
+                strcat(buff, " ");
+	}
+
+	if (sendto(client, buff, strlen(buff), 0, (struct sockaddr *)&remaddr, addrlen) < 0)
+	perror("sendto");
+	
+        memset(buff,0,sizeof(buff));
+	recvfrom(client, buff, sizeof(buff), 0, (struct sockaddr *)&remaddr, &addrlen);
+	printf("\n%s\n",buff);
+	
+	close(client);
+	
+	return 0;
+}
+
