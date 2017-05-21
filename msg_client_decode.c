7eb07fe854cb31e3d12b5f4ffeb852df97bb00d4
Parimalarengan <gprengan90@gmail.com>
2017-05-21 21:09:52 +0530

    Added some changes but not integrated yet
    Change-Id: Iccaab3c650640d136740da48aed24499b40db4d9

---
+++ msg_client_decode.c
@@ -0,0 +1,28 @@
+#include "msg.h"
+
+void msg_decode_client (char *buff, int buff_len)
+{
+    int type,len;
+    type = len = 0;
+    get_type_len(buff, &type, &len);
+
+    buff += sizeof(msg_t)+len;
+
+    switch(type)
+    {
+        case MSG_CLIENT_ADD:
+            element_decode_client_add_response(buff,
+                           buff_len-sizeof(msg_t)+len);
+        break;
+
+        case MSG_CLIENT_MESSAGE:
+            element_decode_client_message(buff,
+                           buff_len-sizeof(msg_t)+len);
+        break;
+
+        default:
+            printf("\n Type not found");
+            break;
+    }
+    return;
+}
