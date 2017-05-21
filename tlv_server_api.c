0bb1c69b62a1ea6fd3d82aba2b02b01572dd7c57
Parimalarengan <gprengan90@gmail.com>
2017-05-21 21:09:52 +0530

    Added some changes but not integrated yet
    Change-Id: Iccaab3c650640d136740da48aed24499b40db4d9

---
+++ tlv_server_api.c
@@ -0,0 +1,38 @@
+#include "tlv.h"
+
+void tlv_decode_client_add (char *buff, int msg_len)
+{
+    int len = 0, tlv_len = 0;
+    int type;
+    client_add_t new_client;
+
+    get_type_len(buff, &type, &tlv_len);
+
+    while(buff+len < buff+msg_len)
+    {
+
+        switch (type)
+        {
+            case TLV_CLIENT_ADD:
+                len += tlv_decode_1byte(buff+tlv_len,
+                    (void *) &new_client.action);
+            break;
+
+            case TLV_CLIENT_NAME:
+                len += tlv_decode_string(buff+tlv_len,
+                        (void *)&new_client.nick_name,
+                        tlv_len);
+            break;
+
+            case TLV_CLIENT_MESSAGE:
+                len += tlv_decode_string(buff+tlv_len,
+                        (void *)&new_client.msg,
+                        tlv_len);
+            default:
+                printf("IN FUN:%s INVALID TYPE",__FUNCTION__);
+
+        }
+        get_type_len(buff, &type, &len);
+    }
+    return;
+}
